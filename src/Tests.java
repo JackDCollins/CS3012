c82173ed007a6f6ded823af8aea739c2cf5b4e5e
JackDCollins <collinj6@tcd.ie>
2018-09-24 21:40:34 +0100

    Testing simple prints

--- src/Tests.java
+++ src/Tests.java
@@ -1,11 +1,27 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class Tests
 {
-	@RunWith(JUnit4.class)
-	public class BSTTest
-	{
 
+@Test
+	public void testPrints()
+	{
+	// Check Null/Empty
+			BST<Integer, Integer> tree = new BST<Integer, Integer>();
+			assertEquals("Check Empty Tree print", "()", tree.printKeysInOrder());
+
+			// Check Left
+			tree = new BST<Integer, Integer>();
+			tree.put(30, 30);
+			tree.put(45, 45);
+			assertEquals("Check Left Node", "(()30(()45()))", tree.printKeysInOrder());
+
+			// Check Right
+			tree = new BST<Integer, Integer>();
+			tree.put(10, 10);
+			tree.put(30, 30);
+			assertEquals("Check print in order with right child node", "(()10(()30()))", tree.printKeysInOrder());
 	}
 }
