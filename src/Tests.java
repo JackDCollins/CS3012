72c7069f91a4bede1a632135429023039d14901c
JackDCollins <collinj6@tcd.ie>
2018-09-29 21:05:45 +0100

    Testing getting items from the BST & added back in the @Test identifier

--- src/Tests.java
+++ src/Tests.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -25,6 +26,8 @@ public class Tests
 		assertEquals("Check print in order with right child node", "(()10(()30()))", tree.printKeysInOrder());
 	}
 
+	
+	@Test
 	public void testContains()
 	{
 		BST<Integer, Integer> tree = new BST<Integer, Integer>();
@@ -34,6 +37,50 @@ public class Tests
 		assertEquals("Check Contins 5", true, tree.contains(5));
 
 	}
+	
+	
+	@Test
+	public void testCalls() {
+
+		// Check Empty
+		BST<Integer, Integer> tree = new BST<Integer, Integer>();
+		assertNull("Check Empty", tree.get(1));
+
+		// Check 2 Node tree
+		tree = new BST<Integer, Integer>();
+		tree.put(30, 30);
+		tree.put(50, 50);
+		assertEquals("Check Root 30", "30", String.valueOf(tree.get(30)));
+		assertEquals("Check Solo Right Node", "50", String.valueOf(tree.get(50)));
+		
+		tree = new BST<Integer, Integer>();
+		tree.put(80, 80);
+		tree.put(90, 90);
+		tree.put(40, 40);
+		tree.put(20, 20);
+		tree.put(30, 30);
+		tree.put(70, 70);
+		tree.put(50, 50);
+		tree.put(60, 60);
+
+		tree = new BST<Integer, Integer>();
+		tree.put(10, 10);
+		tree.put(20, 20);
+		tree.put(5, 5);
+		assertEquals("Check Right Node", "20", String.valueOf(tree.get(20)));
+		assertEquals("Check Left Node", "5", String.valueOf(tree.get(5)));
+		assertEquals("Check the ability to get root node with two childs and that is all.", "10",
+				String.valueOf(tree.get(10)));
+
+		
+
+		assertNull("Check no Node", tree.get(0));
+		assertEquals("Check Node Right", "90", String.valueOf(tree.get(90)));
+		assertEquals("Check Left Node", "30", String.valueOf(tree.get(30)));
+		assertEquals("Check Double Node", "40", String.valueOf(tree.get(40)));
+
+	}
+
 	
 	
 }
