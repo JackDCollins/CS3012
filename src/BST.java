1ab9ec4dc2b86d4f5e48ca70fa3874f808546349
JackDCollins <collinj6@tcd.ie>
2018-09-29 21:22:04 +0100

    Added Final BST Tests + Added Step 1 LCA Test Code to test future LCA Code

--- src/BST.java
+++ src/BST.java
@@ -30,6 +30,17 @@ public class BST<Key extends Comparable<Key>, Value>
 			this.N = N;
 		}
 	}
+	
+	
+	
+	public Node getLCA(int i, int j) {
+		
+		
+		
+		return null;
+		
+	
+	}
 
 	// is the symbol table empty?
 	public boolean isEmpty()
@@ -79,6 +90,10 @@ public class BST<Key extends Comparable<Key>, Value>
 
 		return 1 + Math.max(height(node.left), height(node.right));
 	}
+	
+	
+	
+	
 
 	/**
 	 * Search BST for given key. Does there exist a key-value pair with given
@@ -188,32 +203,7 @@ public class BST<Key extends Comparable<Key>, Value>
 		}
 	}
 
-	/**
-	 * Pretty Printing the tree. Each node is on one line -- see assignment for
-	 * details.
-	 *
-	 * @return a multi-line string with the pretty ascii picture of the tree.
-	 */
-	public String prettyPrintKeys()
-	{
-		if (isEmpty()) { return NULL_STRING + NEXT_LINE; }
 
-		return prettyPrint(root, new String()) + NEXT_LINE;
-	}
-
-	private String prettyPrint(Node node, String tmp)
-	{
-		String result = "";
-		if (isNull(node))
-		{
-			result = tmp + NULL_STRING;
-		} else
-		{
-			result = tmp + "-" + node.val + NEXT_LINE + prettyPrint(node.left, tmp + " |") + NEXT_LINE
-					+ prettyPrint(node.right, tmp + "  ");
-		}
-		return result;
-	}
 
 	/**
 	 * Deteles a key from a tree (if the key is in the tree). Note that this
