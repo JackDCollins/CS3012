8c96d14010d6424a4ab4b931bf455794802ab2ae
JackDCollins <collinj6@tcd.ie>
2018-10-15 23:11:58 +0100

    Created new Graph Development - following failed DAG test

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,45 @@
+
 public class Graph
 {
+	Node root;
 
-	public void test()
+	class Node
 	{
+		int value;
+		Node left;
+		Node right;
+
+		Node(int value)
+		{
+			this.value = value;
+			right = null;
+			left = null;
+		}
 	}
 
+	public void add(int value)
+	{
+		root = addRecursive(root, value);
+	}
+
+	private Node addRecursive(Node current, int value)
+	{
+		if (current == null) { return new Node(value); }
+
+		if (value < current.value)
+		{
+			current.left = addRecursive(current.left, value);
+		} else if (value > current.value)
+		{
+			current.right = addRecursive(current.right, value);
+		} else
+		{
+			// value already exists
+			return current;
+		}
+
+		return current;
+	}
+
+	// https://www.baeldung.com/java-binary-tree
 }
