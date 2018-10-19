718da57f082bac0634821559b82d845f02bef94a
JackDCollins <collinj6@tcd.ie>
2018-10-19 23:17:17 +0100

    Developed Put Code for DAG - Passed previously created tests

--- src/DAG.java
+++ src/DAG.java
@@ -3,39 +3,66 @@ public class DAG<Key extends Comparable<Key>, Value>
 {
 
 	private int vertices;
-	private Bag<GraphNode> nodes;
+	private Bag<DAGNode> nodes;
 
 	DAG()
 	{
 		this.vertices = 0;
-		this.nodes = (Bag<GraphNode>) new Bag();
+		this.nodes = (Bag<DAGNode>) new Bag();
 	}
 
-	private class GraphNode
+	private class DAGNode
 	{
 		private Key key;
 		private Value val;
-		public Bag<GraphNode> descendants;
+		public Bag<DAGNode> descendants;
 		public int inDegree;
 
-		public GraphNode(Key key, Value val)
+		public DAGNode(Key key, Value val)
 		{
 			this.key = key;
 			this.val = val;
 			this.inDegree = 0;
-			this.descendants = (Bag<GraphNode>) new Bag();
+			this.descendants = (Bag<DAGNode>) new Bag();
 		}
 
+		public Key key()
+		{
+			return this.key;
+		}
+
+
 	}
-	
-	
-	//returns the size of the graph
-	public int size() {  return nodes.size(); }
+
+	// returns the size of the graph
+	public int size()
+	{
+		return nodes.size();
+	}
 
 	public boolean put(Key key, Value val)
 	{
-		// TODO
+		if (val != null && !contains(key))
+		{
+			nodes.add(new DAGNode(key, val));
+			this.vertices++;
+			return true;
+		}
 		return false;
+	}
+
+	public boolean contains(Key key)
+	{
+		boolean r =  get(key) != null;
+		return r;
+	}
+
+	public Value get(Key key)
+	{
+		Value answer = null;
+		if (key != null) for (DAGNode node : nodes)
+			if (key.equals(node.key())) answer = node.val;
+		return answer;
 	}
 
 	public boolean insertEdge(Key key1, Key key2)
@@ -43,10 +70,11 @@ public class DAG<Key extends Comparable<Key>, Value>
 		// TODO
 		return false;
 	}
-	
-	private boolean isDAGCycle(GraphNode source, GraphNode dest){
+
+	private boolean isDAGCycle(DAGNode source, DAGNode dest)
+	{
 		// TODO
-	    return false;
-	  }
+		return false;
+	}
 
 }
